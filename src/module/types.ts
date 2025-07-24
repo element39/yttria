5dcdc53599a1ecd8e2122db2bdf8d4daf40ec671
grngxd <36968271+grngxd@users.noreply.github.com>
2025-07-24 22:55:16 +0100

    delete half the language 😪

--- src/module/types.ts
+++
@@ -1,8 +0,0 @@
-import { Expression, ImportExpression, ProgramExpression } from "../parser/ast";
-
-export type ResolvedModule = {
-    files: string[];
-    merged: ProgramExpression;
-    imports: ImportExpression[];
-    exports: Expression[];
-}
